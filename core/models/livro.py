e08dd27aac0886a1c040a8fb7ab6b919421e2cdc
higortab <higor.tabraga@gmail.com>
2026-03-20 10:30:50 -0300

    feat: criação do relacionamento entre livro e categoria

--- core/models/livro.py
+++ core/models/livro.py
@@ -5,6 +5,7 @@ class Livro(models.Model):
     autor = models.ForeignKey('Autor', on_delete=models.CASCADE)
     data_publicacao = models.DateField()
     isbn = models.CharField(max_length=13, unique=True)
+    categoria = models.ForeignKey('Categoria', on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
 
     def __str__(self):
         return self.titulo
