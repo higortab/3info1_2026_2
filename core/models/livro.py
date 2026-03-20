3dc8e5007c364e95e40ed4ab826ffb6b6863d2e1
higortab <higor.tabraga@gmail.com>
2026-03-20 10:38:28 -0300

    feat: criação do relacionamento entre livro e editora

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,11 +1,26 @@
+import re
+
 from django.db import models
+
+from .categoria import Categoria
+from .editora import Editora
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     autor = models.ForeignKey('Autor', on_delete=models.CASCADE)
     data_publicacao = models.DateField()
     isbn = models.CharField(max_length=13, unique=True)
-    categoria = models.ForeignKey('Categoria', on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    categoria = models.ForeignKey(
+        'Categoria', on_delete=models.PROTECT, 
+        related_name='livros', 
+        null=True, 
+        blank=True)
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT,
+        related_name='livros',
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.titulo
